e9647b7e2919ff88d1cd6328bdd0360b46662e50
ajcodeskills <martinaj9514@gmail.com>
2021-07-15 22:58:08 +0530

    First look of my portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of js mastery
+      </SectionText>
+      <Button onClick = {() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+
+  </Section>
+);
+
+export default Hero;
